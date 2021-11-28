5d357971036738c2101e9f9f9a1605507c5a50c5
GitHub Action <action@github.com>
2021-11-28 10:51:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/feimengwang/testideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.feimengwang.testideaplugin.services
+
+import com.github.feimengwang.testideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/feimengwang/testideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/feimengwang/testideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.feimengwang.testideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.feimengwang.testideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
